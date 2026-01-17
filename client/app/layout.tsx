2eea5748ef9c7da78783eb74bf8c6a3980e082da
Soniya-Thapa <ayinos.apaht143@gmail.com>
2026-01-17 13:19:37 +0545

    removed unnecessary folders

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import StoreProvider from "@/lib/redux/store.provider";
-import ToastProvider from '@/lib/components/toast.provider';
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,10 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <StoreProvider>
           {children}
-          <ToastProvider />
-        </StoreProvider>
       </body>
     </html>
   );
