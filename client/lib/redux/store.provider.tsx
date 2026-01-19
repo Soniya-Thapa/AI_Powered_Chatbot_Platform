546faeddd5963b92baff6b0fdda2469a026e8867
Soniya-Thapa <ayinos.apaht143@gmail.com>
2026-01-19 13:03:25 +0545

    storeprovider fixed

--- client/lib/redux/store.provider.tsx
+++ client/lib/redux/store.provider.tsx
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { makeStore, AppStore } from './store';
 
 export default function StoreProvider({ children }: { children: React.ReactNode }) {
-  const storeRef = useRef<AppStore>();
+const storeRef = useRef<AppStore | null>(null);
   
   if (!storeRef.current) {
     storeRef.current = makeStore();
